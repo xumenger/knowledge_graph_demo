e97e213cfd501c2aa7f6d03f4090b4b12198de70
xumenger <xumenger@126.com>
2018-08-26 11:48:00 +0800

    neo4j mysql command modify

--- data/dump.sh
+++ data/dump.sh
@@ -1,8 +1,8 @@
 
 # dump into neo4j
-neo4j-import --into [your-path-to-database]/kgDemo --nodes person_node.txt --nodes company_node.txt --relationships management_edge.txt --delimiter TAB
+neo4j-import --into graph.db/kgDemo --nodes person_node.txt --nodes company_node.txt --relationships management_edge.txt --delimiter TAB
 
 # dump into mysql
-mysql -u root -p[your-password] < dump.sql
+mysql --local-infile -u root -p < dump.sql
 
 
